757dca7c2d96b55e20a62c06b563e33b559ddee9
Gabriel <>
2018-04-17 16:17:50 +0300

    Added buttons Added onClick methods of EditText and num buttons

--- app/src/main/java/com/gabriel/gacalculator/MainActivity.java
+++ app/src/main/java/com/gabriel/gacalculator/MainActivity.java
@@ -1,7 +1,13 @@
-2package com.gabriel.gacalculator;
+package com.gabriel.gacalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        EditText editText = (EditText) findViewById(R.id.calcEditText);
+//        editText.setImeOptions(EditorInfo.IME_ACTION_DONE);
+//        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//                if ( (actionId == EditorInfo.IME_ACTION_DONE) || ((event.getKeyCode() == KeyEvent.KEYCODE_ENTER) && (event.getAction() == KeyEvent.ACTION_DOWN ))) {
+//
+//                    // Do stuff when user presses enter
+//
+//                    return true;
+//
+//                }
+//
+//                return false;
+//            }
+//        });
+
+    }
+
+
+    public void btnClicked(View view){
+
+    }
+
+
+    public void textFiledClicked(View view){
+        Toast.makeText(getApplicationContext(), "Some massage", Toast.LENGTH_LONG).show();
+
+        EditText editText = (EditText) findViewById(R.id.calcEditText);
+        editText.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        editText.setText("");
     }
 }
