9a9e362546241d1eba2ada1cde8f1f2ac0cc889d
Gabriel <>
2018-04-30 17:27:26 +0300

    Refctored and added a recycle list for all the assignments as main.

--- app/src/main/java/com/gabriel/gacalculator/MainActivity.java
+++ app/src/main/java/com/gabriel/gacalculator/MainActivity.java
@@ -2,142 +2,45 @@ package com.gabriel.gacalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements MainListAdapter.ItemClickListener {
 
+    MainListAdapter adapter;
 
-    private String operation = "";
-    private String num1 = "";
-    private String num2 = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activty_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Assignment 1: Calculator");
+        animalNames.add("Assignment 2: Birthday list");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.mainRecycleView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MainListAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
     }
 
 
-    public void btnClicked(View view){
-
-        TextView textView = (TextView) findViewById(R.id.textView);
-
-
-        switch (view.getId()) {
-            case R.id.btn0:
-                addDigitToTextField("0");
-                break;
-            case R.id.btn1:
-                addDigitToTextField("1");
-                break;
-            case R.id.btn2:
-                addDigitToTextField("2");
-                break;
-            case R.id.btn3:
-                addDigitToTextField("3");
-                break;
-            case R.id.btn4:
-                addDigitToTextField("4");
-                break;
-            case R.id.btn5:
-                addDigitToTextField("5");
-                break;
-            case R.id.btn6:
-                addDigitToTextField("6");
-                break;
-            case R.id.btn7:
-                addDigitToTextField("7");
-                break;
-            case R.id.btn8:
-                addDigitToTextField("8");
-                break;
-            case R.id.btn9:
-                addDigitToTextField("9");
-                break;
-            case R.id.btnPlus:
-                operation = "+";
-                break;
-            case R.id.btnMinus:
-                operation = "-";
-                break;
-            case R.id.btnMult:
-                operation = "*";
-                break;
-            case R.id.btnDivide:
-                operation = "/";
-                break;
-            case R.id.btnEqual:
-
-
-                Double tempNum;
-
-                if (num1 != null && num2 != null && operation != null ) {
-                    if (operation == "+") {
-                        tempNum = (Double.parseDouble(num1) + Double.parseDouble(num2));
-                        textView.setText(tempNum.toString());
-
-                    } else if (operation == "-") {
-                        tempNum = (Double.parseDouble(num1) - Double.parseDouble(num2));
-                        textView.setText(tempNum.toString());
-
-                    } else if(operation == "*") {
-                        tempNum = (Double.parseDouble(num1) * Double.parseDouble(num2));
-                        textView.setText(tempNum.toString());
-
-                    }else if(operation == "/"){
-                        tempNum = (Double.parseDouble(num1) / Double.parseDouble(num2));
-                        textView.setText(tempNum.toString());
-                    }
-                }
-
-                break;
-
-            case R.id.btnClear:
-
-                textView.setText("");
-
-                num1 = "";
-                num2 = "";
-                operation = "";
-
-                break;
-        }
-
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
 
-//    public void textViewClicked(View view){
-////        Toast.makeText(getApplicationContext(), "Some massage", Toast.LENGTH_LONG).show();
-//
-//        TextView textView = (TextView) findViewById(R.id.textView);
-//        textView.setImeOptions(EditorInfo.IME_ACTION_DONE);
-//        textView.setText("");
-//    }
-
-    private void addDigitToTextField(String digit)
-    {
-        TextView textView = (TextView) findViewById(R.id.textView);
-        String text = textView.getText().toString();
-
-        if (operation == ""){
-            if (num1 == "" && digit == "0"){}
-            else{
-                text += digit;
-                num1 += digit;
-                textView.setText(num1);
-            }
-        }else{
-            if (num2 == "" && digit == "0"){}
-            else{
-                text += digit;
-                num2 += digit;
-                textView.setText(num2);
-            }
-        }
-    }
 }
